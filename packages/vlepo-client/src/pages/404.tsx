bbae46306805551b58b2465552b4cc6045d11f31
Nathaniel <linlilulll@gmail.com>
2021-04-14 00:35:01 +0800

    custom 404 page

---
+++ packages/vlepo-client/src/pages/404.tsx
@@ -0,0 +1,10 @@
+import { Section } from 'src/components/Layout/style';
+import { H1 } from 'src/components/Typography';
+
+const Custom404 = () => (
+  <Section mx="auto" my="auto">
+    <H1>404 - Page not found</H1>
+  </Section>
+);
+
+export default Custom404;
