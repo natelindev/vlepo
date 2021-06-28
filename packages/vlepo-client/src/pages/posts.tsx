103816b2bd627bb470268d82b2ba8a2fbec71e9f
Nathaniel <linlilulll@gmail.com>
2021-06-28 13:34:16 +0800

    [wip]front page change

--- packages/vlepo-client/src/pages/posts.tsx
+++ packages/vlepo-client/src/pages/posts.tsx
@@ -1,11 +1,17 @@
+import { Masonry, useInfiniteLoader } from 'masonic';
 import dynamic from 'next/dynamic';
+import React from 'react';
 import { usePagination, useQuery } from 'relay-hooks';
 import { fetchQuery, graphql } from 'relay-runtime';
-import { pages_Index_BlogQuery } from 'src/__generated__/pages_Index_BlogQuery.graphql';
+import { ArticleCard_post$key } from 'src/__generated__/ArticleCard_post.graphql';
+import { posts_BlogQuery } from 'src/__generated__/posts_BlogQuery.graphql';
+import { posts_Posts$key } from 'src/__generated__/posts_Posts.graphql';
+import { postsPostRefetchQuery } from 'src/__generated__/postsPostRefetchQuery.graphql';
 import { ErrorText } from 'src/components/Input';
-import { Loading } from 'src/components/PlaceHolder';
-import { useMetaData } from 'src/hooks/useMetaData';
+import PlaceHolder, { Loading } from 'src/components/PlaceHolder';
+import { Slogan, SloganContainer } from 'src/components/Slogan';
 import { initEnvironment } from 'src/relay';
+import { margin, MarginProps } from 'styled-system';
 
 import styled from '@emotion/styled';
 
@@ -13,17 +19,30 @@ import type { GetServerSidePropsContext } from 'next';
 
 const ArticleCard = dynamic(() => import('src/components/ArticleCard'), { loading: Loading });
 
-const blogQuery = graphql`
-  query pages_Index_BlogQuery($id: String!) {
-    blog(where: { id: $id }) {
-      ...pages_Index_Posts
-    }
+const PostMasonry = styled(Masonry)`
+  width: 100%;
+  align-self: center;
+  margin-left: auto;
+  margin-right: auto;
+  &:focus {
+    border: none;
+    outline: none;
   }
+` as typeof Masonry;
+
+const PostRow = styled.div<MarginProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 3rem;
+  ${margin}
 `;
 
-const indexPostsFragment = graphql`
-  fragment pages_Index_Posts on Blog {
-    postsConnection(first: 5) @connection(key: "Index_postsConnection") {
+const postFragmentSpec = graphql`
+  fragment posts_Posts on Blog
+  @argumentDefinitions(count: { type: "Int", defaultValue: 10 }, cursor: { type: "String" })
+  @refetchable(queryName: "postsPostRefetchQuery") {
+    postsConnection(first: $count, after: $cursor) @connection(key: "posts_postsConnection") {
       edges {
         node {
           ...ArticleCard_post
@@ -32,6 +51,15 @@ const indexPostsFragment = graphql`
     }
   }
 `;
+
+const blogQuery = graphql`
+  query posts_BlogQuery($id: String!) {
+    blog(where: { id: $id }) {
+      ...posts_Posts
+    }
+  }
+`;
+
 export const getServerSideProps = async ({ req, res }: GetServerSidePropsContext) => {
   const { environment, relaySSR } = initEnvironment(req.cookies.accessToken);
   await new Promise((resolve, reject) => {
@@ -58,16 +86,23 @@ export const getServerSideProps = async ({ req, res }: GetServerSidePropsContext
   };
 };
 
+type MasonryCardProps = { data: ArticleCard_post$key; width: number };
+
+const MasonryCard: React.FC<MasonryCardProps> = (props: MasonryCardProps) => {
+  const { data, width } = props;
+  return <ArticleCard width={`${width}px`} post={data} />;
+};
+
 type PostSectionProps = {
-  blog: pages_Index_Posts$key;
+  blog: posts_Posts$key;
 };
 const PostsSection = (props: PostSectionProps) => {
   const { blog } = props;
 
-  const { data, hasNext } = usePagination<IndexPostRefetchQuery, pages_Index_Posts$key>(
-    indexFragmentSpec,
-    blog!,
-  );
+  const { data, isLoadingNext, hasNext, loadNext } = usePagination<
+    postsPostRefetchQuery,
+    posts_Posts$key
+  >(postFragmentSpec, blog!);
 
   const maybeLoadMore = useInfiniteLoader(() => hasNext && loadNext(10), {
     isItemLoaded: (index) => (data?.postsConnection?.edges?.length ?? 0) - 1 > index,
@@ -76,8 +111,8 @@ const PostsSection = (props: PostSectionProps) => {
   if (!data) return <PlaceHolder />;
 
   return (
-    <IndexRow mx={['0.3rem', '2rem', '6rem']}>
-      <IndexMasonry<ArticleCard_post$key>
+    <PostRow mx={['0.3rem', '2rem', '6rem']}>
+      <PostMasonry<ArticleCard_post$key>
         columnWidth={350}
         items={
           data && data.postsConnection && data.postsConnection.edges
@@ -92,16 +127,14 @@ const PostsSection = (props: PostSectionProps) => {
         onRender={maybeLoadMore}
       />
       {isLoadingNext && <PlaceHolder width="100%" />}
-    </IndexRow>
+    </PostRow>
   );
 };
 
 export default function Home() {
-  const { error, data } = useQuery<pages_Index_BlogQuery>(blogQuery, {
+  const { error, data } = useQuery<posts_BlogQuery>(blogQuery, {
     id: process.env.NEXT_PUBLIC_DEFAULT_BLOG_ID,
   });
-
-  const { subtitle } = useMetaData();
 
   if (error) {
     return <ErrorText>{error.message}</ErrorText>;
@@ -109,7 +142,15 @@ export default function Home() {
 
   return (
     <>
-      <h1>Test</h1>
+      <SloganContainer
+        cursor={{ show: false }}
+        mt={['5rem', '4rem', '3rem']}
+        mb={['4rem', '5rem', '6rem']}
+        fontSize={[2, 3, 4]}
+      >
+        <Slogan>Posts</Slogan>
+      </SloganContainer>
+      {data && <PostsSection blog={data.blog!} />}
     </>
   );
 }
